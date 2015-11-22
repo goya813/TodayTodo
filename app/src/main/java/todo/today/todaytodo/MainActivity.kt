77e5113248070567d7f7fa25adbf75b205b34079
Goya Tomohiro <wasabibeef@gmail.com>
2015-11-22 16:35:25 +0900

    各UI部品の処理を書いた

--- app/src/main/java/todo/today/todaytodo/MainActivity.kt
+++ app/src/main/java/todo/today/todaytodo/MainActivity.kt
@@ -1,10 +1,12 @@
 package todo.today.todaytodo
 
 import android.app.Activity
+import android.content.Context
 import android.support.v7.app.ActionBarActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.*
+import java.util.*
 
 
 public class MainActivity : Activity() {
@@ -12,6 +14,20 @@ public class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var adapter = TodoListAdapter(this, ArrayList<String>())
+        var todoView = findViewById(R.id.todo_view) as ListView
+        todoView.setAdapter(adapter)
+
+        var inputForm = findViewById(R.id.input_form) as EditText
+        var registerBtn = findViewById(R.id.register_button) as Button
+        registerBtn.setOnClickListener { v ->
+            val text = inputForm.getText().toString()
+            adapter.add(text)
+            adapter.notifyDataSetChanged()
+
+            inputForm.setText("")
+        }
     }
 
     public override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -32,5 +48,37 @@ public class MainActivity : Activity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private class TodoListAdapter(context: Context, items: ArrayList<String>) : ArrayAdapter<String>(context, 0, items)
+    {
+        private val mInflater : LayoutInflater
+
+        init
+        {
+            mInflater = LayoutInflater.from(context)
+        }
+
+        override fun getView(pos: Int, convertView: View?, parent: ViewGroup): View?
+        {
+            var view = convertView
+            val item = getItem(pos) as String
+            val textView : TextView
+
+            if (view == null){
+                view = mInflater!!.inflate(R.layout.todo_row, parent, false)
+            }
+
+            textView = view!!.findViewById(R.id.todo_text) as TextView
+            textView.text = item
+
+            val removeBtn = view!!.findViewById(R.id.remove_btn) as Button
+            removeBtn.setOnClickListener { v ->
+                this.remove(item)
+                this.notifyDataSetChanged()
+            }
+
+            return view
+        }
     }
 }
